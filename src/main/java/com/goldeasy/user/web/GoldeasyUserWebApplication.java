715891c77aeaecd445c29e6842489f970e3ff43c
xiejunxing <350757337@qq.com>
2018-10-09 14:26:46 +0800

    init

---
+++ src/main/java/com/goldeasy/user/web/GoldeasyUserWebApplication.java
@@ -0,0 +1,12 @@
+package com.goldeasy.user.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldeasyUserWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldeasyUserWebApplication.class, args);
+	}
+}
